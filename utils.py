91b1a5023c9a3a550c0bc98a120c1190b8702a03
Marco Sin (mas634) <mas634@mail.usask.ca>
2024-06-06 08:17:31 -0600

    Committing to work on later.  Working on ln 79 to reverse the shifts and rotations

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from math import sin,cos
 
 def off_diag_sum(A):
     """ Computes the sum of the off diagonal entries per row and returns it in the form of a vector
@@ -32,6 +33,63 @@ def eigs(A):
     e = np.linalg.eigvals(A)
     return e.real, e.imag
 
+def brauer(A):
+    from math import cos
+    if A.shape[0] != A.shape[1]:
+        raise Exception("Matrix must be square!")
+    
+    
+    N_points = 1_000
+    Rs = off_diag_sum(A)
+    theta = np.linspace(0, 2*np.pi, N_points)
+    N = A.shape[0]
+    
+    # TODO: Optimize this shit here
+    # This is going to be slow as fuck for large matrices
+    # Potential ideas:
+    #   Rewrite this as a library in CPP or some other 'fast' language
+    #   Find another way to compute the boundary, (research alternative ways to do this)
+    #   Potential for vectorization (Time to RTFM, fml)***
+    #   What does industry do?
+    for i in range(N):
+        for j in range(i+1,N):
+            print("ij")
+            w = A[i][i]
+            
+            # Perform a shift
+            A1 = A - w*np.eye(N) # 1 - Shift
+            gamma = -A1[j][j]
+            
+            A2 = gamma*np.eye(N)*A1 # 2 - Rotate the diagonal A[j,j] entry towards the real line
+            alpha = A2[j][j]/2
+            A3 = A2 - alpha*np.eye(N) # 3 - Shift again
+            
+            for t in theta:
+                P = np.polynomial.Polynomial((
+                    alpha**4 - (Rs[i]*Rs[j])**2, # Constant coef
+                    0, # x
+                    -2*alpha**2*cos(2*t), # x^2
+                    0, # x^3
+                    1  # x^4
+                ))
+                
+                tol = 0.0001 
+                soln = P.roots() # Solutions to our rotated and shifted matrix
+                
+                real_roots = soln[soln.imag < tol] # Retrieve real roots only! 
+                
+                for r in real_roots:
+                    z = w*cos(t) + w*sin(t)*j
+                    flag = z.real > 0
+                    
+                    
+                
+                
+    
+    return None
+    
+    
+
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
     
@@ -62,6 +120,8 @@ if __name__ == '__main__':
     
     r, i = eigs(a)
     ax.plot(r,i, 'ro')
+    
+    brauer(a)
     
     
     
