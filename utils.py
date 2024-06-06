c6a68601833672920bc75e7002763aba78a5fe11
Marco Sin (mas634) <mas634@mail.usask.ca>
2024-06-06 05:23:48 -0600

    Added some basic testing code and made a small program to show gerschgorin disks

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def off_diag_sum(A: np.typing.ArrayLike):
+def off_diag_sum(A):
     """ Computes the sum of the off diagonal entries per row and returns it in the form of a vector
 
     Args:
@@ -9,14 +9,14 @@ def off_diag_sum(A: np.typing.ArrayLike):
     Returns:
         vector: Contains the off diagonal sums per row of a matrix
     """
-    if A.shape[0] != A.shape[1] or len(A.shape):
+    if A.shape[0] != A.shape[1]:
         return
     
     A_mod = abs(A)
     
     return A_mod.sum(axis=1) - np.diag(A_mod)
 
-def eigs(A: np.typing.ArrayLike):
+def eigs(A):
     """ Computes the eigenvalues of a (possibly complex-valued) matrix and returns real and imaginary part into two vectors
 
     Args:
@@ -26,14 +26,49 @@ def eigs(A: np.typing.ArrayLike):
         2xn array: first row contains the real part and second row contains the imaginary part
     """
     
-    if A.shape[0] != A.shape[1] or len(A.shape):
+    if A.shape[0] != A.shape[1]:
+        print("Error")
         return
-    
-    return a.real, a.imaginary
+    e = np.linalg.eigvals(A)
+    return e.real, e.imag
 
 if __name__ == '__main__':
-    import matplotlib as plt
+    import matplotlib.pyplot as plt
     
     print("Running test")
+    
+    a = np.vander([1,2,3], 3)
+    
+    cx = a.diagonal().real
+    cy = a.diagonal().imag
+    
+    radii = off_diag_sum(a)
+    
+    n = cx.shape[0]
+    
+    fig, ax = plt.subplots()
+    
+    bbox = (-20, 20)
+    ax.set_xlim(bbox)
+    ax.set_ylim(bbox)
+    
+    for i in range(n):
+        ax.add_patch(
+            plt.Circle(
+                (cx[i], cy[i]),
+                radii[i],
+                color=(0,0,1,0.1)
+            )
+    )
+    
+    r, i = eigs(a)
+    ax.plot(r,i, 'ro')
+    
+    
+    
+    
+    fig.savefig("test")
+    
+    
     
     
